0ae07ce060c2b2015ca1ef26f9617ae98af4caf6
Diogo Moura <diogoodev@gmail.com>
2024-01-26 01:25:28 +0000

    resolução exercicio 3 - fuções

--- I-introdução-à-programação/capitulo-4/exercicio3.py
+++ I-introdução-à-programação/capitulo-4/exercicio3.py
@@ -1,2 +1,37 @@
 # 3. Escreva uma função que receba tres parametros obrigatorios e dois parametros opcionais
 
+def form(nome, idade, hobby, pais='Brasil', ano='2024'):
+  print("================================================")
+  print("Nome: ", nome)
+  print("idade: ", idade)
+  print('Hobby: ', hobby)
+  print("Pais: ", pais)
+  print("Ano: ", ano)
+  print("================================================")
+
+def insert_form():
+   
+   confirma = input("Você deseja prencher todos os campos do formulario? (y/n)") 
+
+   if confirma == 'y' : 
+      print("Prenche os dados a seguir: ")
+      print("================================================")
+      nome = input("Nome: ")
+      idade = input('Idade: ')
+      hobby = input('Hobby: ')
+      pais = input('Pais: ')
+      ano = input('Ano: ')
+      print("================================================")
+      form(nome, idade, hobby, pais, ano)
+   else:
+      print("================================================")
+      print("Prenche os dados a seguir: ")
+      nome = input("Nome: ")
+      idade = input('Idade: ')
+      hobby = input('Hobby: ')
+      print("================================================")
+      form(nome, idade, hobby)
+
+      
+
+insert_form()
